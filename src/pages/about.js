b59790bfd6d16f58a6030de4ffa65694506bae23
aeciolevy <aeciolevy@gmail.com>
2017-07-15 14:11:17 -0300

    Deleted some old code

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,10 +8,6 @@ class About extends Component {
     super(props);
   }
 
-          //   Graduado em Engenharia Mecânica pela UFCG (1997), em Direito pela UEPB (1998), mestrado em Engenharia Mecânica pela UFPB (2008), Doutorado em Engenharia de Produção pela UFRGS (2013), também com especialização em Tecnologia Mineral pela UFPA (2008) e Gestão Empresarial pela PUCCAMP (2003).
-          //   <br/><br/>Herbert Viana Iniciou sua carreira profissional  na AMBEV (1997 a 1999), desenvolvendo atividades no área de Packaging da empresa. Em 1999 ingressou na Mineração Rio do Norte (1999 a 2003), onde atuou nas áreas engenharia e manutenção industrial. Assumiu na Votorantim o cargo de coordenador de manutenção (2003 a 2006), gerenciando a manutenção industrial de uma das unidades de cimento da companhia (CIPASA - PB). Em 2006 iniciou suas atividades na VALE S.A.. Atuou a frente de diversos desafios nas áreas de engenharia, manutenção, produção e automação, como Gerente Geral de Engenharia e Manutenção de equipamentos de mina e Gerente Executivo de Tratamento Mineral, liderou diversas equipes de operação e manutenção no site de Carajás.
-          //   <br/><br/>Em 2016 ingressa na Universidade Federal do Rio Grande do Norte, onde atua do departamento de engenharia de produção (DEP) e no programa de pós graduação em Engenharia de Produção (PEP)
-          // </p>
   render(){
 
     const divStyle = {
