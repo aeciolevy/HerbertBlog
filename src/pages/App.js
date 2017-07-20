10e68fe4ca5cebc4dc7844f2faef3ca42b36c092
aeciolevy <aeciolevy@gmail.com>
2017-07-19 21:46:39 -0300

    Change the home page style

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,51 +1,94 @@
 import React, { Component } from 'react';
-import Navbar from '../components/navbar';
+import style from 'styled-components';
+import { Keyframes, Frame } from 'react-keyframes';
 import { CSSTransitionGroup } from 'react-transition-group';
 import { Link } from 'react-router-dom';
-import { Carousel } from 'react-bootstrap';
+import { Image } from 'react-bootstrap';
+import Navbar from '../components/navbar';
 import db from '../db/article.json';
+
 
 class App extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      classHide: false
+    }
+  }
+
+  componentDidMount() {
+    setTimeout(this.handleHidden.bind(this), 5000)
+  }
+
+  handleHidden() {
+    this.setState({classHide: true })
   }
 
 
   render() {
 
-    const pictures = db.map( elem => {
+    // const pictures = db.map( elem => {
+    //   const img = elem.cover;
+    //   const id = elem.id;
+    //   const title = elem.title;
+
+    //   return (
+    //     <Carousel.Item key={id}>
+    //       <Link to={`article/${id}`} >
+    //         <img alt="900x500" src={img} className="img-responsive home-img"/>
+    //         <Carousel.Caption>
+    //             <h5 className="article-title"> {title} </h5>
+    //         </Carousel.Caption>
+    //       </Link>
+    //     </Carousel.Item>
+    //   );
+    // })
+    //       <CSSTransitionGroup
+    //       transitionName="example"
+    //       transitionAppear={true}
+    //       transitionAppearTimeout={900} >
+
+    //       <Carousel>
+    //         {pictures}
+    //         </Carousel>
+
+    //     </CSSTransitionGroup>
+
+    const articles = db.map( elem => {
       const img = elem.cover;
       const id = elem.id;
-      const title = elem.title;
-      // <img src={'https://static.wixstatic.com/media/f9ad96_675aac161e0043129b7f1b5a02a5f5c4~mv2.jpg/v1/fill/w_1036,h_498,al_c,lg_1,q_85/f9ad96_675aac161e0043129b7f1b5a02a5f5c4~mv2.webp'} className="img-responsive home-img" alt="foto" />
-      // <button className=" btn btn-primary-outline"> Leia Mais </button>
+
       return (
-        <Carousel.Item key={id}>
+        <div className="flex-home-child">
           <Link to={`article/${id}`} >
-            <img alt="900x500" src={img} className="img-responsive home-img"/>
-            <Carousel.Caption>
-                <h5 className="article-title"> {title} </h5>
-            </Carousel.Caption>
+            <Image src={img} style={{height: '180px', width: '170px', cursor: 'pointer'}} circle />
           </Link>
-        </Carousel.Item>
+        </div>
       );
     })
-
 
     return (
       <div>
         <Navbar />
         <div className="jumbotron" >
-          <CSSTransitionGroup
-          transitionName="example"
-          transitionAppear={true}
-          transitionAppearTimeout={900} >
-
-          <Carousel>
-            {pictures}
-            </Carousel>
-
-        </CSSTransitionGroup>
+         { this.state.classHide ?
+          <div>
+          <h2 style={{textAlign: 'center', padding: '40px 0 0 0'}}> Meus últimos artigos </h2>
+          <div className="flex-home">
+            {articles}
+          </div>
+          </div>
+          :
+          <div>
+          <Keyframes component="h1"  className="title-page" >
+            <Frame duration={500}>"Saudações </Frame>
+            <Frame duration={500}>"Saudações a quem </Frame>
+            <Frame duration={500}>"Saudações a quem tem</Frame>
+            <Frame duration={500}>"Saudações a quem tem coragem"</Frame>
+          </Keyframes>
+          <img alt="900x500" src="https://scontent.fplu2-1.fna.fbcdn.net/v/t1.0-9/12552573_557747981044175_6630160594613650282_n.jpg?oh=6c5999cd258212d45b18687c3d34c4ea&oe=5A0054B7" className={`img-responsive picture`}/>
+          </div>
+        }
 
         </div>
       </div>
